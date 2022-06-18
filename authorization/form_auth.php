103f3ade883f2efd1f83765780a00cf1541738b1
SilenceCr1stal <nikitapolyashov2@gmail.com>
2022-06-18 12:35:04 +0300

    added gitignore file

--- authorization/form_auth.php
+++ authorization/form_auth.php
@@ -8,22 +8,22 @@ require_once '../public/templates/head.php';
         <form action="./auth.php" method="POST" name="form_auth">
             <div class="input-group">
                 <div class="inset">
-                    <input autocomplete="off" type="email" name="email_user" required />
-                    <label class="input-label" for="name">
+                    <input autocomplete="off" id="email" type="email" name="email_user" required />
+                    <label class="input-label" for="email">
                         <span class="input-label-content">E-mail</span>
                     </label>
                 </div>
             </div>
             <div class="input-group">
                 <div class="inset">
-                    <input autocomplete="off" type="password" name="password_user" required />
-                    <label class="input-label" for="name">
+                    <input autocomplete="off" id="password" type="password" name="password_user" required />
+                    <label class="input-label" for="password">
                         <span class="input-label-content">Пароль</span>
                     </label>
                 </div>
             </div>
             <div class="input-group btn">
-                <input type="submit" name="btn_submit_auth" value="Войти">
+                <input type="submit" name="btn_submit_auth" value="Войти" id="submit__btn" style="width: 100%; height: 100%;">
             </div>
         </form>
         <a class="login-link" href=<?= $address."/authorization/form_register.php"?>>У меня нет аккаунта</a>
--- authorization/form_register.php
+++ authorization/form_register.php
@@ -10,7 +10,7 @@ require_once '../public/templates/head.php';
         <form action="./register.php" method="POST" name="form_register">
             <div class="input-group">
                 <div class="inset">
-                    <input autocomplete="off" type="text" name="fullName_user" required />
+                    <input autocomplete="off" id="name" type="text" name="fullName_user" required />
                     <label class="input-label" for="name">
                         <span class="input-label-content">Имя</span>
                     </label>
@@ -18,30 +18,30 @@ require_once '../public/templates/head.php';
             </div>
             <div class="input-group">
                 <div class="inset">
-                    <input autocomplete="off" type="text" name="login_user" required />
-                    <label class="input-label" for="name">
+                    <input autocomplete="off" id="login" type="text" name="login_user" required />
+                    <label class="input-label" for="login">
                         <span class="input-label-content">Логин</span>
                     </label>
                 </div>
             </div>
             <div class="input-group">
                 <div class="inset">
-                    <input autocomplete="off" type="email" name="email_user" required />
-                    <label class="input-label" for="name">
+                    <input autocomplete="off" id="email" type="email" name="email_user" required />
+                    <label class="input-label" for="email">
                         <span class="input-label-content">E-mail</span>
                     </label>
                 </div>
             </div>
             <div class="input-group">
                 <div class="inset">
-                    <input autocomplete="off" type="password" name="password_user" required />
-                    <label class="input-label" for="name">
+                    <input autocomplete="off" id="password" type="password" name="password_user" required />
+                    <label class="input-label" for="password">
                         <span class="input-label-content">Пароль</span>
                     </label>
                 </div>
             </div>
             <div class="input-group btn">
-                <input type="submit" name="btn_submit_register" value="Регистрация">
+                <input type="submit" name="btn_submit_register" value="Регистрация" id="submit__btn" style="width: 100%; height: 100%;">
             </div>
         </form>
         <a class="login-link" href=<?= $address."/authorization/form_auth.php"?>>Я уже зарегистрирован</a>
